ff74f1ba9ed7069fe721fc897b2bb47b2b50577a
David Ng <dave@codeaurora.org>
2011-07-05 10:53:34 -0700

    Add hooks for database open and pragma handling
    Change-Id: Icb4e52885cc4b35ffdb09b32dd0bd9975a632d7c

--- dist/Android.mk
+++ dist/Android.mk
@@ -58,6 +58,10 @@ LOCAL_SHARED_LIBRARIES += liblog \
 # include android specific methods
 LOCAL_WHOLE_STATIC_LIBRARIES := libsqlite3_android
 
+ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
+LOCAL_WHOLE_STATIC_LIBRARIES += libqc-sqlite
+LOCAL_SHARED_LIBRARIES += libcutils
+endif
 
 include $(BUILD_SHARED_LIBRARY)
 
